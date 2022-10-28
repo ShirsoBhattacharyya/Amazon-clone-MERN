b159786c4f8ea39ef4ae389eaef0cf8899a48cbb
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2022-10-29 00:35:05 +0530

    routes updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import connect from "./components/config/database";
 import userRoutes from './components/features/routes/user.routes';
 import authRoutes from './components/features/routes/auth.routes';
 import productRoutes from './components/features/routes/product.routes';
+import cartRoutes from './components/features/routes/cart.routes';
+import orderRoutes from './components/features/routes/order.routes';
 const server:Application=express();
 const PORT=process.env.PORT||8080;
 //Middlewares
@@ -10,6 +12,9 @@ server.use(express.json());
 server.use('/api/users',userRoutes);
 server.use('/api/auth',authRoutes);
 server.use('/api/products',productRoutes);
+server.use('/api/carts',cartRoutes);
+server.use('/api/orders',orderRoutes);
+
 server.get('/api/',(req:Request,res:Response)=>{
     res.send('Welcome to Tweek Ecommerce Api.')
 })
